8f7ce5241eb29c684be4c8ec208ba1823091ec21
Bryan Giger <bryan@gigerbyt.es>
2019-02-24 11:53:39 +0100

    create .env.sample

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/wiealtbistdu', function(request, response) {
-
   response.send('hello from Router')
 })
 
@@ -13,5 +12,10 @@ router.post('/', function(req, res) {
 router.get('/evelin', (req, res) => res.send('eveline'))
 
 router.get('/*', (req, res) => res.send('not very specific'))
+
+// TO REFACTOR L8R
+function queryHSGAPI(){
+
+}
 
 export default router
